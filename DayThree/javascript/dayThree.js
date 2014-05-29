19163f798c16eb6c0a1f6dd72a97b279593df008
unknown <comsenzce13@gmail.com>
2014-05-28 20:19:37 -0500

    Fixing runAuthorSelect function.

--- DayThree/javascript/dayThree.js
+++ DayThree/javascript/dayThree.js
@@ -6,6 +6,7 @@ $('document').ready(function(){
 function runAuthorSelect(){
 	var authors = readAlong.getAuthors();
 	_.each(authors, function(author){
+		//fix this up.
 		$('#authorSelect').after('<div class="col-md-4"><div class="author-card "><h3 class="text-center">' + author.lastName + '</h3></div></div>');
 	});
 };
